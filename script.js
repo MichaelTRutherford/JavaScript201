064e3aea1255da9d0a7bd5c80b9b8af137e07205
Michael Rutherford <mtruth65@gmail.com>
2024-02-29 14:30:02 +1000

    Completed Course

---
+++ script.js
@@ -0,0 +1,15 @@
+const object = {
+    'name': 'Michael',
+    'favFoods': ['Pizza', 'tacos', 'steak'],
+    'age': 58,
+    'children': [
+        {
+            'name': 'Kwila',
+            'age': 1,
+        },
+        {
+            'name': 'Acacia',
+            'age': 1
+        }
+    ]
+}
